2c636d13b94f5bd4f3a1b12d19a4dbc5650552f5
Obinna Igwe <igwenus619@gmail.com>
2020-02-18 21:36:55 +0100

    Add bottom bar

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movies_list_app/src/widgets/bottom_app_bar.dart';
 
 
 class MyHomeScreen extends StatefulWidget {
@@ -9,13 +10,7 @@ class MyHomeScreen extends StatefulWidget {
   _MyHomeScreenState createState() => _MyHomeScreenState();
 }
 class _MyHomeScreenState extends State<MyHomeScreen> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  int _currentTabIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -23,25 +18,27 @@ class _MyHomeScreenState extends State<MyHomeScreen> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
+      bottomNavigationBar: AppBottomNavigationBar(
+        onTabSelected: _changeTab,
+        items: [
+          AppBottomNavigationBarItem(iconData: Icons.whatshot, text: 'All'),
+          AppBottomNavigationBarItem(iconData: Icons.star, text: 'Favourites'),
+        ],
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
     );
+  }
+
+
+  void _changeTab(int nextTabIndex) {
+    setState(() {
+      _currentTabIndex = nextTabIndex;
+    });
   }
 }
