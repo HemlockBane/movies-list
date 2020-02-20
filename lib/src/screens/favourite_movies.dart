82723d8a337bd3515170b679354aa4f8ad548826
Obinna Igwe <igwenus619@gmail.com>
2020-02-20 23:03:52 +0100

    Show favourite movies in favourite movies screen

--- lib/src/screens/favourite_movies.dart
+++ lib/src/screens/favourite_movies.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:movies_list_app/src/notifiers/popular_movies_notifier.dart';
+import 'package:movies_list_app/src/widgets/movie_card.dart';
+import 'package:provider/provider.dart';
 
 class FavouriteMoviesScreen extends StatefulWidget {
   @override
@@ -13,7 +16,16 @@ class _FavouriteMoviesScreenState extends State<FavouriteMoviesScreen> {
         title: Text('Favourite Movies'),
       ),
       body: Container(
-        child: Center(child: Text('Favourites')),
+        child: Consumer<PopularMoviesNotifier>(
+          builder: (context, moviesNotifier, _){
+            return ListView.builder(
+              itemCount: moviesNotifier.favouriteMovies.length,
+              itemBuilder: (context, itemIndex){
+                final dummyFavouriteMovie = moviesNotifier.favouriteMovies[itemIndex];
+                return MovieCard(movie: dummyFavouriteMovie,);
+              });
+          },
+        ),
       ),
     );
   }
