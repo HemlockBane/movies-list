d685a97b866245bd42373b692dcf376fd2dfb108
Obinna Igwe <igwenus619@gmail.com>
2020-02-20 23:07:07 +0100

    Refactor code

--- lib/src/notifiers/popular_movies_notifier.dart
+++ lib/src/notifiers/popular_movies_notifier.dart
@@ -4,20 +4,23 @@ import 'package:flutter/material.dart';
 import 'package:movies_list_app/src/models/movie.dart';
 import 'package:provider/provider.dart';
 
-class PopularMoviesNotifier with ChangeNotifier{
+class PopularMoviesNotifier with ChangeNotifier {
   static PopularMoviesNotifier of(BuildContext context) =>
-    Provider.of<PopularMoviesNotifier>(context, listen: false);
+      Provider.of<PopularMoviesNotifier>(context, listen: false);
 
+  bool _isLoadingMovies = false;
   List<Movie> _movies = getDummyMovies();
 
+  bool get isLoadingMovies => _isLoadingMovies;
 
   UnmodifiableListView<Movie> get allMovies => UnmodifiableListView(_movies);
-  UnmodifiableListView<Movie> get favouriteMovies => UnmodifiableListView(_movies.where((movie) => movie.isFavourite));
 
-  void toggleFavourite({Movie movie}){
+  UnmodifiableListView<Movie> get favouriteMovies =>
+      UnmodifiableListView(_movies.where((movie) => movie.isFavourite));
+
+  void toggleFavourite({Movie movie}) {
     final movieIndex = _movies.indexOf(movie);
     _movies[movieIndex].toggleFavourite();
     notifyListeners();
   }
-
 }
